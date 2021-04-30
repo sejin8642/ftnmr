f3b4e0eb83dafcfffc725eb862273746730aa05b
Sejin Nam <sejin8642@gmail.com>
2021-04-29 18:27:35 -1000

    added note.txt and modified fid.py

--- ft/fid.py
+++ ft/fid.py
@@ -1,20 +1,29 @@
 # simple free induction decay code
+# References
+"""
+Keeler pg.49 for general signal form
+proton gyromagnetic ratio: https://physics.nist.gov/cgi-bin/cuu/Value?gammap
+"""
 import numpy as np
 
 class fid():
-    def __init__(self, B=1.5, T2=2000, timeunit='msec'):
-        pass
+    def __init__(self, B=1.5, shift=0, T2=2000, timeunit='msec'):
         self.B = B #approximately 2000 msec is T2 for water/CSF at 1.5T
-        self.t = np.linspace(1, 6*T2, 6*T2)
+        self.shift = shift
         self.T2 = T2
-        self.gamma = 267.52219*pow(10,6)
+        self.gamma = 267.52218744*pow(10,6)
+
+        # Signal frequency is adjusted according to the chemical shift
         if timeunit == 'msec':
-            self.w = 0.001*self.gamma*self.B
+            self.w = pow(10, -9)*self.shift*self.gamma*self.B
         elif timeunit == 'micron':
-            self.w = 0.000001*self.gamma*self.B
+            self.w = pow(10, -12)*self.shift*self.gamma*self.B
         else:
             raise Exception('Incorrect time unit is specified')
 
-    def __call__(self):
-        return np.exp(-self.t/self.T2)*np.cos(self.t)
+        # Below sampling rate samples approximately 20 in one period of larmor precession
+        self.SR = round(0.31415/self.w, int(-np.log10(0.31415/self.w)) + 1)
+        self.t = np.linspace(0, 6*T2 - self.SR, int(6*T2/self.SR))
 
+    def __call__(self):
+        return np.exp(-self.t/self.T2)*(np.cos(self.w*self.t) + 1j*np.sin(self.w*self.t))
